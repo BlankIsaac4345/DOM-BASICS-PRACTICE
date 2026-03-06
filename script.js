676cd97f29c63adec3188acbc6f19b575c7f6447
BlankIsaac4345 <isaacml4345@gmail.com>
2026-03-06 18:46:15 +0000

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Script started");
+
+function changecolor() {
+    console.log("click")
+    let color = prompt("Enter a new color for the peom");
+    let poem = document.getElementById("poem");
+    poem.style.color = color;
+}
